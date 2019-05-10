60bfc115d70e3f94590aec927d6f2208bc5bd9eb
Daniel Agurcia <danielagurcia90@gmail.com>
2019-05-10 15:19:53 -0600

    commit 4

--- lab4.cpp
+++ lab4.cpp
@@ -5,10 +5,13 @@ using std::endl;
 #include <string>
 using std::string;
 
+//metodos
 char** CrearMatriz(int,int);
 void LiberarMatriz(char**,int);
 void ImprimirMatriz(char**,int,int);
-
+int Validar(string);
+void LLenarMatriz(char**,string,int,int);
+//main
 int main(){
 	char resp='s';
 	while(resp=='s'||resp=='S'){
@@ -25,6 +28,20 @@ int main(){
 		cout<< endl;
 		cout<< "Ingrese en un string el patron de la primera fila: ";
 		cin>> patron;
+		int vali = Validar(patron);
+		cout<<"vali: "<<vali<<endl;
+		cout<<"tam: "<<patron.length()<<endl;
+		while(patron.length()!=columnas||vali==0){
+			if(patron.length()!=columnas){
+				cout<< "El patron tiene que tene la misma longitud que sus columnas("<<columnas<<")"<<endl;
+				cout<< "Ingrese en un string el patron de la primera fila: ";
+                		cin>> patron;
+			}else{
+				cout<< "El patron tiene caracteres incorrectos"<<endl;
+                        	cout<< "Ingrese en un string el patron de la primera fila: ";
+                        	cin>> patron;
+			}
+		}
 		cout<< endl;
 		matrix=CrearMatriz(filas,columnas);
 		ImprimirMatriz(matrix,filas,columnas);
@@ -36,10 +53,14 @@ return 0;
 }
 
 char** CrearMatriz(int filas,int columnas){
-	char** matrix = new char*[columnas];
+	char** matrix = new char*[filas];
 	for(int i=0;i<filas;i++){
 		matrix[i]=new char[columnas];
 	}
+	for(int j = 0;j<filas;j++)
+		for(int k = 0; k<columnas;k++)
+			matrix[j][k]=' ';
+
 	return matrix;
 }
 void LiberarMatriz(char** matrix,int size){
@@ -49,6 +70,7 @@ void LiberarMatriz(char** matrix,int size){
 			matrix[i]=NULL;
 		}
 	}
+	delete[] matrix;
 }
 void ImprimirMatriz(char** matrix,int filas, int columnas){
 	for(int i=0;i<filas;i++){
@@ -58,4 +80,30 @@ void ImprimirMatriz(char** matrix,int filas, int columnas){
 		cout<<endl;
 	}
 
+}
+int Validar(string cadena){
+	int vali=1;
+	for(int i=0;i<cadena.length();i++){
+		cout<<"cadena["<<i<<"]= "<<cadena[i]<<endl;
+		if(cadena[i]!='*'&&cadena[i]!='^'){
+			vali=0;
+		}
+	}
+	cout<<"vali: "<<vali<<endl;
+	return vali;
+}
+void LLenarMatriz(char** matrix,string patron,int filas, int columnas){
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<columnas;j++){
+			if(i==0){
+				matrix[i][j]=patron[j];
+			}
+			if(j==1){
+
+			}
+			if(j==patron.length()-1){
+
+			}
+		}
+	}
 }
